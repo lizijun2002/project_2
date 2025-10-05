28d680c7a8944c39fa486fb86a3e09c8f0d9e128
lizijun2002 <165897715+lizijun2002@users.noreply.github.com>
2025-10-05 00:49:06 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -15,25 +15,27 @@ public class Main {
 
         System.out.println("Output of part 2");
         System.out.println("================");
-        System.out.println("Result of expr1: " + expr1.accept(new ExprEvaluator()));
-        System.out.println("Result of expr2: " + expr2.accept(new ExprEvaluator()));
-        System.out.println("Result of expr3: " + expr3.accept(new ExprEvaluator()));
+        ExprVisitor<Float> ev = new ExprEvaluator();
+        System.out.println("Result of expr1: " + expr1.accept(ev));
+        System.out.println("Result of expr2: " + expr2.accept(ev));
+        System.out.println("Result of expr3: " + expr3.accept(ev));
     }
 
+    // 3 + 2*5 = 13.0
     public static Expr mkExpr1() {
-        // 3 + 2*5 = 13
-        return new PlusExpr(new FloatExpr(3), new TimesExpr(new FloatExpr(2), new FloatExpr(5)));
+        return new PlusExpr(new FloatExpr(3f),
+                            new TimesExpr(new FloatExpr(2f), new FloatExpr(5f)));
     }
 
+    // 1/2 + 3/8 = 0.875
     public static Expr mkExpr2() {
-        // 1/2 + 3/8 = 0.875
-        return new PlusExpr(new DivExpr(new FloatExpr(1), new FloatExpr(2)),
-                            new DivExpr(new FloatExpr(3), new FloatExpr(8)));
+        return new PlusExpr(new DivExpr(new FloatExpr(1f), new FloatExpr(2f)),
+                            new DivExpr(new FloatExpr(3f), new FloatExpr(8f)));
     }
 
+    // 1/2 - 4/5 = -0.3
     public static Expr mkExpr3() {
-        // 1/2 - 4/5 = -0.3
-        return new MinusExpr(new DivExpr(new FloatExpr(1), new FloatExpr(2)),
-                             new DivExpr(new FloatExpr(4), new FloatExpr(5)));
+        return new MinusExpr(new DivExpr(new FloatExpr(1f), new FloatExpr(2f)),
+                             new DivExpr(new FloatExpr(4f), new FloatExpr(5f)));
     }
 }
